f70a3d9134a681aad221ce255830e2c653501382
hsyou <realhs01@gmail.com>
2018-09-03 10:39:26 +0900

    first commit

---
+++ src/main/java/com/example/twitter2/Twitter2Application.java
@@ -0,0 +1,12 @@
+package com.example.twitter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Twitter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Twitter2Application.class, args);
+	}
+}
